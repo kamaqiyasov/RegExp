28938bf9d0cadc9734666f9325ca2652f02eebc1
kamaqiyasov <kamil181994@gmail.com>
2025-11-02 16:04:25 +0300

    (last commit)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import os
 import re
@@ -39,43 +37,47 @@ def format_phone(phone_number):
     subst = "+7(\\2)\\3-\\4-\\5\\6\\8\\9"
     return re.sub(regex, subst, phone_number, 0)
     
-with open(os.path.join(os.getcwd(), "phonebook_raw.csv"), encoding="utf-8") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
+def main():
+    with open(os.path.join(os.getcwd(), "phonebook_raw.csv"), encoding="utf-8") as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
 
-# TODO 1: выполните пункты 1-3 ДЗ
-#  lastname,firstname,surname,organization,position,phone,email
-phonebooks = []
-header = contacts_list[0]
-for row in contacts_list[1:]:
-    fio = " ".join(row[:3]).split()    
-    lastname = fio[0]
-    firstname = fio[1]
-    employee = PhoneBook(lastname, firstname)
+    # TODO 1: выполните пункты 1-3 ДЗ
+    #  lastname,firstname,surname,organization,position,phone,email
+    phonebooks = []
+    header = contacts_list[0]
+    for row in contacts_list[1:]:
+        fio = " ".join(row[:3]).split()    
+        lastname = fio[0]
+        firstname = fio[1]
+        employee = PhoneBook(lastname, firstname)
 
-    if len(fio) > 2:
-        employee.surname = fio[2]
+        if len(fio) > 2:
+            employee.surname = fio[2]
+            
+        employee.organization = row[3]
+        employee.position = row[4]
+        employee.phone = format_phone(row[5])
+        employee.email = row[6]
         
-    employee.organization = row[3]
-    employee.position = row[4]
-    employee.phone = format_phone(row[5])
-    employee.email = row[6]
-    
-    employee_exist = None
-    for phonebook in phonebooks:
-        if employee == phonebook:
-            employee_exist = phonebook
-            break
+        employee_exist = None
+        for phonebook in phonebooks:
+            if employee == phonebook:
+                employee_exist = phonebook
+                break
+            
+        if employee_exist:
+            employee_exist.merge_info(employee)
+        else:
+            phonebooks.append(employee)        
         
-    if employee_exist:
-        employee_exist.merge_info(employee)
-    else:
-        phonebooks.append(employee)        
-    
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-with open(os.path.join(os.getcwd(), "phonebook.csv"), "w", newline='', encoding="utf-8") as f:
-    datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerow(header)
-    for phonebook in phonebooks:
-        datawriter.writerow(phonebook.get_list())
+    # TODO 2: сохраните получившиеся данные в другой файл
+    # код для записи файла в формате CSV
+    with open(os.path.join(os.getcwd(), "phonebook.csv"), "w", newline='', encoding="utf-8") as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerow(header)
+        for phonebook in phonebooks:
+            datawriter.writerow(phonebook.get_list())
+        
+if __name__ == "__main__":
+    main()
